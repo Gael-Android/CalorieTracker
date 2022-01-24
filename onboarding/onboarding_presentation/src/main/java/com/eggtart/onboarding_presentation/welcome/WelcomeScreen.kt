b289ac67f35e6f896ad2c617dde4c16235fed3b4
Gael-Android <gael.urssu@gmail.com>
2022-01-24 22:26:07 +0900

    navigation util

---
+++ onboarding/onboarding_presentation/src/main/java/com/eggtart/onboarding_presentation/welcome/WelcomeScreen.kt
@@ -0,0 +1,48 @@
+package com.eggtart.onboarding_presentation.welcome
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.eggtart.core.navigation.Route
+import com.eggtart.core.util.UiEvent
+import com.eggtart.core_ui.LocalSpacing
+import com.eggtart.onboarding_presentation.R
+import com.eggtart.onboarding_presentation.components.ActionButton
+
+@Composable
+fun WelcomeScreen(
+    onNavigate: (UiEvent.Navigate) -> Unit
+) {
+    val context = LocalContext.current
+    val spacing = LocalSpacing.current
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(spacing.spaceMedium),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = stringResource(id = R.string.welcome_text),
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.h1
+        )
+        Spacer(modifier = Modifier.height(spacing.spaceMedium))
+        ActionButton(
+            text = stringResource(id = R.string.next),
+            onClick = {
+                onNavigate(UiEvent.Navigate(Route.AGE))
+            },
+            modifier = Modifier.align(CenterHorizontally)
+        )
+    }
+}
